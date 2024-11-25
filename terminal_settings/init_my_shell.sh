8724b46682c608a1e5ff623dd35b4f5b87c3c021
Hyuk Min <judicious210@gmail.com>
2024-11-26 02:16:53 +0900

    Add .bashrc file for automatic launch of zsh

--- terminal_settings/init_my_shell.sh
+++ terminal_settings/init_my_shell.sh
@@ -53,7 +53,7 @@ fi
 # 6. 파일을 홈 디렉토리로 심볼릭 링크 생성
 echo "[6] Creating symbolic links for configuration files in home directory..."
 
-FILES=(.gitconfig .my_aliases.sh .vimrc .zshrc .p10k.zsh)
+FILES=(.gitconfig .my_aliases.sh .vimrc .zshrc .bashrc .p10k.zsh)
 for FILE in "${FILES[@]}"; do
     if [ -f "$FILE" ]; then
         if [ -e "$HOME/$FILE" ]; then
